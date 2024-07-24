91f17d8bd339c6bc3e5c8910d51bcc1a1a28bc42
Grigoriy Patrushev <grigoriy.patrushev@nutnet.ru>
2024-07-24 16:02:32 +0400

    composer update

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ define('DEV_MODE', true);
 // define('DEV_MODE', FALSE);
 try {
     try {
-        require_once 'bootstrap\bootstrap.php';
+        // require_once 'bootstrap\bootstrap.php';
+        require_once __DIR__ . '/vendor/autoload.php';
     } catch (Error $e) {
         if (DEV_MODE === true) {
             $exceptionType = str_replace('Exception\\', '', get_class($e));
